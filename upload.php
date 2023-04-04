33c4dceea3fc7b7fc499981435184992b53ab02c
Usman Moazzam <usmanmoazzam@hotmail.com>
2023-04-04 15:56:06 -0400

    altered php to check for csv

--- upload.php
+++ upload.php
@@ -2,15 +2,15 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// check if image file is a csv
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+  $check = mime_content_type($_FILES["fileToUpload"]["tmp_name"]);
+  if($check == "text/csv") {
+    echo "File is a csv - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo "File is not a csv.";
     $uploadOk = 0;
   }
 }
