438445a20e40855e2cac48912e30b720a6e9d199
Usman Moazzam <usmanmoazzam@hotmail.com>
2023-04-05 20:43:51 -0400

    added code to check uploadOk and submit file

--- upload.php
+++ upload.php
@@ -15,7 +15,6 @@ if(isset($_POST["submit"])) {
     $uploadOk = 0;
   }
 }
-
 // limit file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
@@ -26,5 +25,17 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
+  }
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
   }
 ?>
