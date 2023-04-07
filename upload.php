0d42a0b9f740e0c1b89b6c8c35b98673b1049c88
Usman Moazzam <usmanmoazzam@hotmail.com>
2023-04-07 15:49:02 -0400

    added comments to php and html

--- upload.php
+++ upload.php
@@ -6,6 +6,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $header = true;
         $data = [];
+
+        // reads submitted csv file
         while (($row = fgetcsv($handle, 1000, ",")) !== false) {
             if ($header) {
                 $header = false;
@@ -16,19 +18,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         fclose($handle);
 
+        // calculates average of each column
         $averages = array_fill(0, count($data[0]), 0);
-
         foreach ($data as $row) {
             foreach ($row as $index => $value) {
                 $averages[$index] += $value;
             }
         }
-
         $num_rows = count($data);
         foreach ($averages as &$avg) {
             $avg = $avg / $num_rows;
         }
 
+        // returns average of each column as json
         echo json_encode($averages);
     } else {
         echo "Error uploading file.";
